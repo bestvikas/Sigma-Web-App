47fba9af81464d0e80c0fbc40c5edaea3f7ed2bd
vivekkumbhar2710 <vivekkumbhar27102001@gmail.com>
2024-06-04 14:30:28 +0530

    web form add in app

---
+++ sigmaarocos/sigma_arocos/doctype/job_opening/job_opening.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Vikas Deshmukh and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Job Opening', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ sigmaarocos/sigma_arocos/doctype/job_opening/test_job_opening.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Vikas Deshmukh and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestJobOpening(FrappeTestCase):
+	pass
